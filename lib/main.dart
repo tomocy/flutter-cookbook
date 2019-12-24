af5dec28ba0064ccf986ed4afbf1d5c0b112c5e8
tomocy <tomocy.dev@gmail.com>
2019-12-24 15:49:37 +0900

    conduct widget tests

--- lib/main.dart
+++ lib/main.dart
@@ -9,87 +9,34 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Page(
-        client: http.Client(),
+        title: 't',
+        message: 'm',
       ),
     );
   }
 }
 
-class Page extends StatefulWidget {
+class Page extends StatelessWidget {
   const Page({
     Key key,
-    @required this.client,
+    @required this.title,
+    @required this.message,
   }) : super(key: key);
 
-  final http.Client client;
-
-  @override
-  _PageState createState() => _PageState();
-}
-
-class _PageState extends State<Page> {
-  Future<Post> _post;
-
-  @override
-  void initState() {
-    super.initState();
-    _post = fetchPost(widget.client);
-  }
+  final String title;
+  final String message;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Post'),
+        title: Text(title),
       ),
       body: Center(
-        child: FutureBuilder<Post>(
-          future: _post,
-          builder: (context, snapshot) {
-            if (snapshot.hasData)
-              return ListTile(
-                title: Text(snapshot.data.title),
-                subtitle: Text(snapshot.data.body),
-              );
-            if (snapshot.hasError) return Text(snapshot.error.toString());
-
-            return const CircularProgressIndicator();
-          },
-        ),
+        child: Text(message),
       ),
     );
   }
-}
-
-Future<Post> fetchPost(http.Client client) async {
-  final response =
-      await client.get('https://jsonplaceholder.typicode.com/posts/1');
-  if (response.statusCode != 200) {
-    throw Exception('failed to fetch post');
-  }
-
-  return Post.fromJson(json.decode(response.body));
-}
-
-class Post {
-  const Post({
-    this.id,
-    this.userId,
-    this.title,
-    this.body,
-  });
-
-  factory Post.fromJson(Map<String, dynamic> json) => Post(
-        id: json['id'],
-        userId: json['userId'],
-        title: json['title'],
-        body: json['body'],
-      );
-
-  final int id;
-  final int userId;
-  final String title;
-  final String body;
 }

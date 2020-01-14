b12114620be162ba2ffce12e300917b3a596d4eb
tomocy <tomocy.dev@gmail.com>
2020-01-14 20:57:25 +0900

    update MovieTile

--- lib/pages/widgets/movie_tile.dart
+++ lib/pages/widgets/movie_tile.dart
@@ -4,34 +4,39 @@ import '../../models/movie.dart';
 class MovieTile extends StatelessWidget {
   const MovieTile({
     Key key,
+    this.onTap,
     @required this.movie,
   })  : assert(movie != null),
         super(key: key);
 
+  final VoidCallback onTap;
   final Movie movie;
 
   @override
-  Widget build(BuildContext context) => Stack(
-        fit: StackFit.expand,
-        children: [
-          Card(
-            margin: const EdgeInsets.all(0),
-            child: Image.network(
-              movie.posterUri,
-              fit: BoxFit.cover,
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: onTap,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            Card(
+              margin: const EdgeInsets.all(0),
+              child: Image.network(
+                movie.posterUri,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            color: Colors.black.withOpacity(0.5),
-            child: Text(
-              movie.title,
-              style: Theme.of(context)
-                  .textTheme
-                  .title
-                  .copyWith(color: Colors.white),
+            Container(
+              alignment: Alignment.center,
+              color: Colors.black.withOpacity(0.5),
+              child: Text(
+                movie.title,
+                style: Theme.of(context)
+                    .textTheme
+                    .title
+                    .copyWith(color: Colors.white),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
 }

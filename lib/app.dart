379f44cfa427a4af07b8379e5477472f5b4c9439
tomocy <tomocy.dev@gmail.com>
2020-01-14 20:51:47 +0900

    update App

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'blocs/resources/movies_fetcher.dart';
 import 'pages/movies_page.dart';
+import 'resources/movies_fetcher.dart';
 
 class App extends StatelessWidget {
   const App({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(home: MoviesPage());
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          Provider<MoviesFetcher>(create: (_) => fetchMockMovies),
+        ],
+        child: MaterialApp(home: MoviesPage.create()),
+      );
 }

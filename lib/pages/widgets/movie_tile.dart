0626176423ddb6931b0bebaf3fdfcdaf173d00d4
tomocy <tomocy.dev@gmail.com>
2020-01-14 20:50:33 +0900

    add MovieTile

---
+++ lib/pages/widgets/movie_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import '../../models/movie.dart';
+
+class MovieTile extends StatelessWidget {
+  const MovieTile({
+    Key key,
+    @required this.movie,
+  })  : assert(movie != null),
+        super(key: key);
+
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context) => Stack(
+        fit: StackFit.expand,
+        children: [
+          Card(
+            margin: const EdgeInsets.all(0),
+            child: Image.network(
+              movie.posterUri,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            color: Colors.black.withOpacity(0.5),
+            child: Text(
+              movie.title,
+              style: Theme.of(context)
+                  .textTheme
+                  .title
+                  .copyWith(color: Colors.white),
+            ),
+          ),
+        ],
+      );
+}

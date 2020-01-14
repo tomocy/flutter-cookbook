a33be81057d9a5c2c3fd99571ab37d80c33d38ce
tomocy <tomocy.dev@gmail.com>
2020-01-14 20:50:23 +0900

    add MoviesBloc

---
+++ lib/blocs/movies_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import '../models/movie.dart';
+import 'resources/movies_fetcher.dart';
+
+class MoviesBloc {
+  MoviesBloc(this._fetch) {
+    _fetchController.stream.listen(_invokeFetch);
+  }
+
+  final MoviesFetcher _fetch;
+  final _moviesController = StreamController<List<Movie>>();
+  final _fetchController = StreamController<void>();
+
+  Stream<List<Movie>> get movies => _moviesController.stream;
+
+  Sink<void> get fetch => _fetchController.sink;
+
+  Future<void> _invokeFetch(void _) async {
+    try {
+      final movies = await _fetch();
+      _moviesController.add(movies);
+    } on MoviesFetchException catch (e) {
+      _moviesController.addError(e.toString());
+    }
+  }
+
+  void dispose() {
+    _moviesController.close();
+    _fetchController.close();
+  }
+}

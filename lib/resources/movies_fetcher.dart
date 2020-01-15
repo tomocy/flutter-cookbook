835785833ec09e869eaf4109e81cc4aa696f26ca
tomocy <tomocy.dev@gmail.com>
2020-01-15 21:18:52 +0900

    add fetchMoviesFromTmdb

--- lib/resources/movies_fetcher.dart
+++ lib/resources/movies_fetcher.dart
@@ -1,5 +1,25 @@
+import 'dart:convert';
 import 'dart:math';
+import 'package:http/http.dart';
+import '../blocs/resources/movies_fetcher.dart';
 import '../models/movie.dart';
+
+final _tmdbApiKey = '802b2c4b88ea1183e50e6b285a27696e';
+
+Future<List<Movie>> fetchMoviesFromTmdb() async {
+  final response = await get(
+      'https://api.themoviedb.org/3/discover/movie?api_key=$_tmdbApiKey');
+  if (response.statusCode != 200) {
+    throw MoviesFetchException();
+  }
+
+  final body = json.decode(response.body) as Map<String, dynamic>;
+  final jsons = body['results'] as List<dynamic>;
+
+  return jsons
+      .map((json) => Movie.fromJson(json as Map<String, dynamic>))
+      .toList();
+}
 
 Future<List<Movie>> fetchMockMovies() async => List<Movie>.generate(
       100,

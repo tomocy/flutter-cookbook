3f01657b56f08dbe224fcecfc54f73f75a48cf82
tomocy <tomocy.dev@gmail.com>
2020-01-16 19:31:58 +0900

    remove

--- lib/blocs/resources/movies_fetcher.dart
+++
@@ -1,5 +0,0 @@
-import '../../models/movie.dart';
-
-typedef MoviesFetcher = Future<List<Movie>> Function();
-
-class MoviesFetchException implements Exception {}
--- lib/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final ThemeData light = ThemeData.light().copyWith(
-  colorScheme: const ColorScheme.light(),
-);
-
-final ThemeData dark = ThemeData.dark().copyWith(
-  colorScheme: const ColorScheme.dark(),
-);

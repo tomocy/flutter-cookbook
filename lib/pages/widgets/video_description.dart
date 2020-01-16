6e3c7f30f54d2e2702a116ccee395e327c7186f8
tomocy <tomocy.dev@gmail.com>
2020-01-16 21:03:44 +0900

    modify VideoDescription

--- lib/pages/widgets/video_description.dart
+++ lib/pages/widgets/video_description.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 
 class VideoDescription extends StatelessWidget {
-  const VideoDescription({
-    Key key,
-    @required this.height,
-  }) : super(key: key);
-
-  final double height;
+  const VideoDescription({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => SizedBox(
-        height: height,
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              '@tomocy',
-              style: Theme.of(context)
-                  .textTheme
-                  .caption
-                  .copyWith(fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                '@tomocy',
+                style: Theme.of(context)
+                    .textTheme
+                    .caption
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
             ),
-            Text('Video title', style: Theme.of(context).textTheme.caption),
-            Text('Song title', style: Theme.of(context).textTheme.caption),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Video title',
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Song title',
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ),
           ],
         ),
       );

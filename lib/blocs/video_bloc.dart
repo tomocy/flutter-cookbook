670fd3cc3dcce661a82f306940e1c8dbae260543
tomocy <tomocy.dev@gmail.com>
2020-01-17 04:26:27 +0900

    add VideoBloc

---
+++ lib/blocs/video_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:cookbook/domain/models/video.dart';
+import 'package:cookbook/domain/resources/video_fetcher.dart';
+
+class VideoBloc {
+  VideoBloc(this._fetch) {
+    _fetchController.stream.listen(_invokeFetch);
+  }
+
+  final VideoFetcher _fetch;
+  final _videoController = StreamController<Video>();
+  final _fetchController = StreamController<String>();
+
+  Stream<Video> get video => _videoController.stream;
+
+  Sink<String> get fetch => _fetchController.sink;
+
+  Future<void> _invokeFetch(String title) async {
+    try {
+      final video = await _fetch(title);
+      _videoController.add(video);
+    } on VideoFetcherException catch (e) {
+      _videoController.addError(e);
+    }
+  }
+
+  void dispose() {
+    _videoController.close();
+    _fetchController.close();
+  }
+}

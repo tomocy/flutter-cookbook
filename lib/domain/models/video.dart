f177088cdaab4947d3cf5625a847023997a90a32
tomocy <tomocy.dev@gmail.com>
2020-01-18 02:21:33 +0900

    modify Video

--- lib/domain/models/video.dart
+++ lib/domain/models/video.dart
@@ -1,11 +1,15 @@
+import 'package:cookbook/domain/models/song.dart';
+
 class Video {
   const Video({
     this.user,
     this.title,
+    this.song,
   });
 
   final String user;
   final String title;
+  final Song song;
 
   String get userId => '@$user';
 }
